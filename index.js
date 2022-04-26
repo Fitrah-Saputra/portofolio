06ca08e9a926e5b269d29f8d9c2e432585b1c450
Fitrah Saputra <59692416+Fitrah-Saputra@users.noreply.github.com>
2022-04-26 16:20:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
-// const express   = require('express');
-// const app       = express();
-// const port      = 3000;
+const express   = require('express');
+const app       = express();
+const port      = 3000;
 
-// app.use(express.static('public'))
-// app.use('/css', express.static(__dirname + 'public/css'))
-// app.use('/img', express.static(__dirname + 'public/img'))
-// app.use('/js', express.static(__dirname + 'public/js'))
-// app.use('/vendor', express.static(__dirname + 'public/vendor'))
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/img', express.static(__dirname + 'public/img'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/vendor', express.static(__dirname + 'public/vendor'))
 
-// app.get('/', (req, res) => {
-  //   res.sendFile(__dirname + '/view/index.html')
-  // })
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/view/index.html')
+  })
   
-  // app.listen(port, () => {
-    //   console.log('Listening on port ' + port);
-    // })
-
-
-const express = require("express");
-const product = require('./api/product');
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use('/api/product', product);
-
-app.listen(PORT, () => {
-  console.log(`Server is running in port ${PORT}`);
-})
+  app.listen(port, () => {
+      console.log('Listening on port ' + port);
+    })
